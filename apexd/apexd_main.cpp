bananadroid/android_system_apex

commit 5368879048d0f6750c2008c9ab47963bd15fb468

Move call to BootCompletedCleanup from OnBootCompleted into apexd_main
OnBootCompleted implements markBootCompleted binder, which is
synchronously called by StagingManager during boot to notify apexd of
the fact that staged sessions have been applied, and hence apexd should
stop monitoring for native crashes (now it's job of PackageWatchdog).

Because of that, we should don't any heavy work inside OnBootCompleted.
Instead, all the heavy cleanup work is now triggered from apexd_main
before calling AllowServiceShutdown to make sure that servicemanager
won't kill apexd in the middle of the cleanup routine.

Bug: 192426053
Test: atest ApexTestCases
Change-Id: I45a23035bea3fda27a421a17e18792f2aa0a2de6

apexd/apexd_main.cpp
@@ -190,6 +190,9 @@ int main(int /*argc*/, char** argv) {
     lifecycle.WaitForBootStatus(android::apex::RevertActiveSessionsAndReboot);
   }
 
+  // Run cleanup routine before AllowServiceShutdown(), to prevent
+  // service_manager killing apexd in the middle of the cleanup.
+  android::apex::BootCompletedCleanup();
   android::apex::binder::AllowServiceShutdown();
 
   android::apex::binder::JoinThreadPool();

apexd/apexd.cpp
@@ -1958,7 +1958,6 @@ void DeleteDePreRestoreSnapshots(const ApexSession& session) {
 
 void OnBootCompleted() {
   ApexdLifecycle::GetInstance().MarkBootCompleted();
-  BootCompletedCleanup();
 }
 
 // Returns true if any session gets staged

yaap/system_apex

commit d527c8811055182233af12491d2becdf72be5031

Run BootCompletedCleanup() on boot complete
When both BootCompletedCleanup() and installAndActivatePackage() run
simultaneously, BootCompletedCleanup() can remove the target package
while it's being installed. (or remove DM device).

This is a regression from 5368879048d0f6750c2008c9ab47963bd15fb468,
where BootCompletedCleanup() was moved from OnBootCompleted() to
apexd_main. Before the change, BootCompletedCleanup() was called only
when markBootComplete() is invoked. But with the change,
BootCompletedCleanup() is called even when apexservice is started as a
lazy service.

While the race could be fixed in many ways, restricting
BootCompletedCleanup() to run only when "booting" might be easy one.

Bug: 260143932
Test: atest ApexTestCases
Test: atest VendorApexHostTestCases
Change-Id: I4ccafb4883c361eee5e6882809f7502a1718577c

apexd/apexd_main.cpp
@@ -177,6 +177,8 @@ int main(int /*argc*/, char** argv) {
     //  ApexFileRepository can act as cache and re-scanning is not expensive
     android::apex::InitializeDataApex();
   }
+  // start apexservice before ApexdLifecycle::WaitForBootStatus which waits for
+  // IApexService::markBootComplete().
   android::apex::binder::CreateAndRegisterService();
   android::apex::binder::StartThreadPool();
 
@@ -196,11 +198,12 @@ int main(int /*argc*/, char** argv) {
     // logic to make sure that service_manager won't kill apexd in the middle of
     // loop configuration.
     result.wait();
+    // Run cleanup routine on boot complete.
+    // This should run before AllowServiceShutdown() to prevent
+    // service_manager killing apexd in the middle of the cleanup.
+    android::apex::BootCompletedCleanup();
   }
 
-  // Run cleanup routine before AllowServiceShutdown(), to prevent
-  // service_manager killing apexd in the middle of the cleanup.
-  android::apex::BootCompletedCleanup();
   android::apex::binder::AllowServiceShutdown();
 
   android::apex::binder::JoinThreadPool();