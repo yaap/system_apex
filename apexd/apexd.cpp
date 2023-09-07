9062d7f5a4965d994ffae741d8f4894eb64a56cc
Thiébaud Weksteen <tweek@google.com>
2023-09-07 13:06:02 +1000

    Remove support for SEPolicy APEX
    Bug: 297794885
    Test: atest ApexServiceTestCases
    Test: atest ApexTestCases
    Change-Id: I3d284976507c6af64f5ed84ddba2673e9f4a3f6c

--- apexd/apexd.cpp
+++ apexd/apexd.cpp
@@ -706,92 +706,6 @@ Result<void> Unmount(const MountedApexData& data, bool deferred) {
 
 namespace {
 
-// TODO(b/218672709): get the ro.build.version.sdk version of the device.
-const auto kSepolicyLevel = std::to_string(__ANDROID_API_T__);
-const auto kVersionedSepolicyZip = "SEPolicy-" + kSepolicyLevel + ".zip";
-const auto kVersionedSepolicySig = "SEPolicy-" + kSepolicyLevel + ".zip.sig";
-const auto kVersionedSepolicyFsv =
-    "SEPolicy-" + kSepolicyLevel + ".zip.fsv_sig";
-
-const auto kSepolicyZip = "SEPolicy.zip";
-const auto kSepolicySig = "SEPolicy.zip.sig";
-
-Result<void> CopySepolicyToMetadata(const std::string& mount_point) {
-  LOG(DEBUG) << "Copying SEPolicy files to /metadata/sepolicy/staged.";
-  const auto policy_dir = mount_point + "/etc";
-
-  // Find SEPolicy zip and signature files.
-  std::optional<std::string> sepolicy_zip;
-  std::optional<std::string> sepolicy_sig;
-  std::optional<std::string> sepolicy_fsv;
-  auto status =
-      WalkDir(policy_dir, [&sepolicy_zip, &sepolicy_sig, &sepolicy_fsv](
-                              const std::filesystem::directory_entry& entry) {
-        if (!entry.is_regular_file()) {
-          return;
-        }
-        const auto& path = entry.path().string();
-        if (base::EndsWith(path, kVersionedSepolicyZip)) {
-          sepolicy_zip = path;
-        } else if (base::EndsWith(path, kVersionedSepolicySig)) {
-          sepolicy_sig = path;
-        } else if (base::EndsWith(path, kVersionedSepolicyFsv)) {
-          sepolicy_fsv = path;
-        }
-      });
-  if (!status.ok()) {
-    return status.error();
-  }
-  if (sepolicy_zip->empty() || sepolicy_sig->empty() || sepolicy_fsv->empty()) {
-    return Error() << "SEPolicy files not found.";
-  }
-  LOG(INFO) << "SEPolicy files found.";
-
-  // Set up staging directory.
-  std::error_code ec;
-  const auto staged_dir =
-      std::string(gConfig->metadata_sepolicy_staged_dir) + "/";
-  status = CreateDirIfNeeded(staged_dir, 0755);
-  if (!status.ok()) {
-    return status.error();
-  }
-
-  // Clean up after myself.
-  auto scope_guard = android::base::make_scope_guard([&staged_dir]() {
-    std::error_code ec;
-    std::filesystem::remove_all(staged_dir, ec);
-    if (ec) {
-      LOG(WARNING) << "Failed to clear " << staged_dir << ": " << ec.message();
-    }
-  });
-
-  // Copy files to staged folder.
-  const auto stagedSepolicyZip = staged_dir + kSepolicyZip;
-  std::map<std::string, std::string> from_to = {
-      {*sepolicy_zip, stagedSepolicyZip},
-      {*sepolicy_sig, staged_dir + kSepolicySig}};
-  for (const auto& [from, to] : from_to) {
-    std::filesystem::copy_file(
-        from, to, std::filesystem::copy_options::update_existing, ec);
-    if (ec) {
-      return Error() << "Failed to copy " << from << " to " << to << ": "
-                     << ec.message();
-    }
-  }
-
-  status = enableFsVerity(stagedSepolicyZip);
-  if (!status.ok()) {
-    // TODO(b/218672709): once we have a release certificate available, return
-    // an error and make the ApexdMountTest#CopySepolicyToMetadata test pass.
-    LOG(ERROR) << status.error().message();
-  } else {
-    LOG(INFO) << "fs-verity enabled on " << stagedSepolicyZip;
-  }
-
-  scope_guard.Disable();
-  return {};
-}
-
 template <typename VerifyFn>
 Result<void> RunVerifyFnInsideTempMount(const ApexFile& apex,
                                         const VerifyFn& verify_fn,
@@ -929,8 +843,6 @@ Result<void> VerifyPackageBoot(const ApexFile& apex_file) {
   return {};
 }
 
-static constexpr auto kSepolicyApexName = "com.android.sepolicy.apex";
-
 // A version of apex verification that happens on SubmitStagedSession.
 // This function contains checks that might be expensive to perform, e.g. temp
 // mounting a package and reading entire dm-verity device, and shouldn't be run
@@ -942,9 +854,6 @@ Result<void> VerifyPackageStagedInstall(const ApexFile& apex_file) {
   }
 
   const auto validate_fn = [&apex_file](const std::string& mount_point) {
-    if (apex_file.GetManifest().name() == kSepolicyApexName) {
-      return CopySepolicyToMetadata(mount_point);
-    }
     if (IsVendorApex(apex_file)) {
       return CheckVendorApexUpdate(apex_file, mount_point);
     }
@@ -1689,20 +1598,6 @@ Result<ApexFile> GetActivePackage(const std::string& packageName) {
   return ErrnoError() << "Cannot find matching package for: " << packageName;
 }
 
-Result<void> DeleteStagedSepolicy() {
-  const auto staged_dir =
-      std::string(gConfig->metadata_sepolicy_staged_dir) + "/";
-  LOG(DEBUG) << "Deleting " << staged_dir;
-  std::error_code ec;
-  auto removed = std::filesystem::remove_all(staged_dir, ec);
-  if (removed == 0) {
-    LOG(INFO) << staged_dir << " already deleted.";
-  } else if (ec) {
-    return Error() << "Failed to clear " << staged_dir << ": " << ec.message();
-  }
-  return {};
-}
-
 /**
  * Abort individual staged session.
  *
@@ -1712,15 +1607,6 @@ Result<void> AbortStagedSession(int session_id) {
   auto session = gSessionManager->GetSession(session_id);
   if (!session.ok()) {
     return Error() << "No session found with id " << session_id;
-  }
-
-  const auto& apex_names = session->GetApexNames();
-  if (std::find(std::begin(apex_names), std::end(apex_names),
-                kSepolicyApexName) != std::end(apex_names)) {
-    const auto result = DeleteStagedSepolicy();
-    if (!result.ok()) {
-      return result.error();
-    }
   }
 
   switch (session->GetState()) {
@@ -3064,7 +2950,6 @@ void OnStart() {
   const auto& all_apex = instance.AllApexFilesByName();
   // There can be multiple APEX packages with package name X. Determine which
   // one to activate.
-  // TODO(b/218672709): skip activation of sepolicy APEX during boot.
   auto activation_list = SelectApexForActivation(all_apex, instance);
 
   // Process compressed APEX, if any
